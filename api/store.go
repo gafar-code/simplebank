8b3a88b784934d4a9be24a0043c437d5b34e0029
gafar-code <gafartok@gmail.com>
2023-10-30 21:03:02 +0700

    Handle Err Empty Balance

--- api/store.go
+++ api/store.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"errors"
 	"net/http"
 
 	db "github.com/gafar-code/simplebank/db/sqlc"
@@ -24,6 +25,18 @@ func (server *Server) transferTx(ctx *gin.Context) {
 		FromAccountID: req.FromAccountID,
 		ToAccountID:   req.ToAccountID,
 		Amount:        req.Amount,
+	}
+
+	fromAccount, err := server.store.GetAccountForUpdate(ctx, arg.FromAccountID)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	if fromAccount.Balance < arg.Amount {
+		err := errors.New("saldo tidak cukup")
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
 	}
 
 	account, err := server.store.TransferTx(ctx, arg)
