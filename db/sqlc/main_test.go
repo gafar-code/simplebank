a9e54efe531bb5afba3bdb5498e63b9c74476c1e
gafar-code <gafartok@gmail.com>
2023-10-30 07:36:07 +0700

    Fix test

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:52522/simple_bank?sslmode=disable"
+	dbSource = "postgresql://root:secret@localhost:56779/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
